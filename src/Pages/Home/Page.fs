421a2160a14ec40d2f30cdcd0ab7e9ccc8bbcaed
robin jacobsson <robin.jacobsson@kebne.se>
2023-12-07 09:13:15 +0100

    Run elmish land init command

---
+++ src/Pages/Home/Page.fs
@@ -0,0 +1,22 @@
+module GittyGUI.Pages.Home.Page
+
+open System
+open Feliz
+open Elmish
+
+type Model = unit
+
+type Msg = | NoOp
+
+let init (): Model * Cmd<Msg> =
+    (),
+    Cmd.none
+
+let update (msg: Msg) (model: Model): Model * Cmd<Msg> =
+    match msg with
+    | NoOp -> model, Cmd.none
+
+let view (model: Model) (dispatch: Msg -> unit): ReactElement =
+    Html.text "Home"
+
+let subscribe (model: Model) : (string list * ((Msg -> unit) -> IDisposable)) list = []
