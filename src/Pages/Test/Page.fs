4a59fb96cc83185b327cce40f0b3c97b9d303d4c
robin jacobsson <robin.jacobsson@kebne.se>
2023-12-26 00:06:22 +0100

    Add test page

---
+++ src/Pages/Test/Page.fs
@@ -0,0 +1,24 @@
+module TestingElmishLand.Pages.Test.Page
+
+open System
+open Feliz
+open Elmish
+open TestingElmishLand
+open TestingElmishLand.Shared
+
+type Model = unit
+
+type Msg = | NoOp
+
+let init (shared: SharedModel) (): Model * Command<Msg, SharedMsg> =
+    (),
+    Command.none
+
+let update (shared: SharedModel) (msg: Msg) (model: Model): Model * Command<Msg, SharedMsg> =
+    match msg with
+    | NoOp -> model, Command.none
+
+let view (shared: SharedModel) (model: Model) (dispatch: Msg -> unit): ReactElement =
+    Html.text "Test"
+
+let subscribe (model: Model) : (string list * ((Msg -> unit) -> IDisposable)) list = []
